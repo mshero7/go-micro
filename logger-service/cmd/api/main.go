eb201e4f5eea08c4e84bc2f211834fd1ebe069a6
sangsu moon <mshero7@naver.com>
2023-07-20 23:52:09 +0900

    mongo create, docker-compose edit

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"log-service/data"
+	"net/http"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -19,6 +22,7 @@ const (
 var client *mongo.Client
 
 type Config struct {
+	Models data.Models
 }
 
 func main() {
@@ -41,6 +45,37 @@ func main() {
 		}
 	}()
 
+	// 주입.
+	app := Config{
+		Models: data.New(client),
+	}
+
+	// start Web Server
+	// go app.serve()
+	log.Println("Starting service on port", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Println(err)
+		log.Panic()
+	}
+}
+
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic()
+	}
 }
 
 func connectToMongo() (*mongo.Client, error) {
@@ -57,6 +92,8 @@ func connectToMongo() (*mongo.Client, error) {
 		log.Println("Error connecting:", err)
 		return nil, err
 	}
+
+	log.Println("Connect to Mongo")
 
 	return c, nil
 }
